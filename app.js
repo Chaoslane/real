1be89d0b2ceb6bec96cc96b6d8ec06453102034f
chaoslane <chaoslanes@gmail.com>
2017-09-04 17:58:53 +0800

    fix bugs

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const server = require('http').Server(app.callback());
 const rhconf = require('./conf/realhook.json');
 const syncConfig = require('./lib/sync_config')(rhconf);
 
+
 // middleware
 const koaBody = require('koa-body');
 const Router = require('koa-router');
@@ -19,14 +20,8 @@ const realstatus = require('./lib/mid_realstatus');
 app.use(koaBody());
 app.use(router.routes());
 app.use(router.allowedMethods());
-// router 调用中间件
 router.post(rhconf.realhook.stat_path, realstatus.keepReal());
 // router.post(rhconf.realhook.stat_path, senttcp());
-
-server.listen(rhconf.realhook.stat_port);
-server.on('error', onError);
-server.on('listening', onListening);
-
 
 
 // --------------socket.io--------------------
@@ -34,45 +29,17 @@ const io = require('socket.io')(server);
 const realhook = io.of('/realhook');
 
 realhook.on('connection', (socket) => {
+    log.info(`New connection from: ${socket.handshake.address}`);
     setInterval(function () {
         socket.emit('realdata', realstatus.status);
     }, 1000);
+    socket.on('disconnect', (reason) => {
+        log.info(reason);
+    });
+    socket.on('error', (error) => {
+        log.error(error);
+    });
 });
 
 
-
-/**
- * Event listener for HTTP server "error" event.
- */
-function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-    const bind = typeof port === 'string'
-        ? 'Pipe ' + port
-        : 'Port ' + port;
-    // handle specific listen errors with friendly messages
-    switch (error.code) {
-        case 'EACCES':
-            log.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            log.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-function onListening() {
-    const addr = server.address();
-    const bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-    log.info('Realhook listening on ' + bind);
-}
+module.exports = server;
