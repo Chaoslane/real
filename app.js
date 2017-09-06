b70239b8cef1018a3dcb281b4e153ab669538c20
chaoslane <chaoslanes@gmail.com>
2017-09-06 18:28:08 +0800

    fix bugs

--- app.js
+++ app.js
@@ -15,16 +15,19 @@ const koaBody = require('koa-body');
 const Router = require('koa-router');
 const router = new Router();
 const realstatus = require('./lib/mid_realstatus');
-// const senttcp = require('./mid_sendtcp');
 
 app.use(koaBody());
 app.use(router.routes());
 app.use(router.allowedMethods());
 router.post(rhconf.realhook.stat_path, realstatus.keepReal());
-// router.post(rhconf.realhook.stat_path, senttcp());
+
 
 // response
-app.use(async ctx => {ctx.body = "OK";});
+router.post(rhconf.realhook.stat_path, async ctx => {ctx.body = "OK";});
+// error handler
+app.on('error', function(err,ctx){
+    log.error(err.message);
+});
 
 // --------------socket.io--------------------
 const io = require('socket.io')(server);
@@ -33,7 +36,7 @@ const realhook = io.of('/realhook');
 realhook.on('connection', (socket) => {
     log.info(`New connection from: ${socket.handshake.address}`);
     socket.on('getstatus', () => {
-        socket.emit('realdata', realstatus.status);
+        socket.emit('status', realstatus.status);
     });
     socket.on('disconnect', (reason) => {
         log.info(`Client exit: ${reason}`);
